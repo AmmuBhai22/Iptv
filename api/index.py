fcaf3545b8068e99c895e2836421416097bb94f5
AmmuBhai22 <109035027+AmmuBhai22@users.noreply.github.com>
2023-06-20 01:58:22 +0530

    Rollback to old code

--- api/index.py
+++ api/index.py
@@ -17,7 +17,6 @@ def jso():
     url="http://tv.trexiptv.com/portal.php?type=itv&action=get_all_channels&force_ch_link_check=&JsHttpRequest=1-xml"
     token=requests.get(request.base_url.replace("/json","/token")).text
     headers={"User-Agent":"Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3","X-User-Agent":"Model: MAG250; Link: WiFi","Referer":"http://tv.trexiptv.com/c/","Cookie":"mac=00:1A:79:5A:1C:B8; stb_lang=en; timezone=GMT","Accept":"*/*","Host":"tv.trexiptv.com","Connection":"Keep-Alive","Accept-Encoding":"gzip","Authorization":"Bearer "+str(token)}
-    
     data=requests.get(url,headers=headers).json()
     #fin={}
     js={"type": "list","headline": "Videos","template": {"tag": "Web","type": "separate","layout": "0,0,2,4","icon": "live-tv", "color": "msx-glass"}}
@@ -28,35 +27,11 @@ def jso():
             dt.append({"title":dota["name"],"playerLabel":dota["name"],"action":"video:plugin:http://msx.benzac.de/plugins/hls.html?url="+request.base_url.replace("/json","/m3u8/")+dota["cmd"].replace("ffmpeg ","").replace("?","piyush").replace("&","aman").replace("http://","http:/")})
         js["items"]=dt
     final=make_response(js)
-    #final.headers["Content-Type"] = "application/json; charset=utf-8"
+    final.headers["Content-Type"] = "application/json; charset=utf-8"
     final.headers["Access-Control-Allow-Origin"]="*"
     final.headers["Access-Control-Allow-Headers"]="Origin, Content-Type, Accept"
     final.headers["Access-Control-Allow-Methods"]="GET, OPTIONS"
     return final
-
-@app.route("/tata")
-def tata():
-    url="http://tv.trexiptv.com/portal.php?type=itv&action=get_all_channels&force_ch_link_check=&JsHttpRequest=1-xml"
-    token=requests.get(request.base_url.replace("/json","/token")).text
-    headers={"User-Agent":"Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3","X-User-Agent":"Model: MAG250; Link: WiFi","Referer":"http://tv.trexiptv.com/c/","Cookie":"mac=00:1A:79:5A:1C:B8; stb_lang=en; timezone=GMT","Accept":"*/*","Host":"tv.trexiptv.com","Connection":"Keep-Alive","Accept-Encoding":"gzip","Authorization":"Bearer "+str(token)}
-    
-    data=requests.get(url,headers=headers).json()
-    #fin={}
-    js={"type": "list","headline": "Videos","template": {"tag": "Web","type": "separate","layout": "0,0,2,4","icon": "live-tv", "color": "msx-glass"}}
-    dt=[]
-    #for i in range(0,len(data)):
-    for dota in data["js"]["data"]:
-        if dota["tv_genre_id"]=="1283":
-            dt.append({"title":dota["name"],"playerLabel":dota["name"],"action":"video:plugin:http://msx.benzac.de/plugins/hls.html?url="+request.base_url.replace("/json","/m3u8/")+dota["cmd"].replace("ffmpeg ","").replace("?","piyush").replace("&","aman").replace("http://","http:/")})
-        js["items"]=dt
-    final=make_response(js)
-    #final.headers["Content-Type"] = "application/json; charset=utf-8"
-    final.headers["Access-Control-Allow-Origin"]="*"
-    final.headers["Access-Control-Allow-Headers"]="Origin, Content-Type, Accept"
-    final.headers["Access-Control-Allow-Methods"]="GET, OPTIONS"
-    
-    return final
-
 
 @app.route("/m3u8/<path:link>")
 def playlist(link):
@@ -81,4 +56,4 @@ def tss(ts):
     final.headers["Access-Control-Allow-Headers"]="Origin, Content-Type, Accept"
     final.headers["Access-Control-Allow-Methods"]="GET, OPTIONS"
     return final
-#app.run()
+app.run()
